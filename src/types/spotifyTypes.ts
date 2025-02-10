1aef6fc532e75aad32447c62825432b23ff1692d
vikram(wsl) <vikramjit360@gmail.com>
2025-02-10 21:23:58 +0530

    feat: add types for Spotify request errors

--- src/types/spotifyTypes.ts
+++ src/types/spotifyTypes.ts
@@ -1,9 +1,15 @@
-export interface SpotifyAuthenticationError {
-  error: string;
-  error_description: string;
-}
+import { z } from "zod";
 
-export interface SpotifyError {
-  status: string;
-  message:  string;
-}
+export const SpotifyAuthenticationErrorSchema = z.object({
+  error: z.string(),
+  error_description: z.string(),
+});
+
+export type SpotifyAuthenticationError = z.infer<typeof SpotifyAuthenticationErrorSchema>;
+
+export const SpotifyErrorSchema = z.object({
+  status: z.string(),
+  message: z.string(),
+});
+
+export type SpotifyError = z.infer<typeof SpotifyErrorSchema>;
