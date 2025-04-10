76a9197f90f4b3f4b9863ed210f83588914ea907
vikram(wsl) <vikramjit360@gmail.com>
2025-04-10 12:17:06 +0530

    feat: add and lauch lambda function on aws

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -39,3 +39,81 @@ resource "aws_dynamodb_table" "state_verifier_pair" {
     Environment = "Production"
   }
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+
+  tags = {
+    Project     = "TuneTally_Terraform"
+    Environment = "Production"
+  }
+}
+
+resource "aws_iam_policy" "lambda_dynamodb_access" {
+  name = "lambda_dynamodb_access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ],
+        Resource = aws_dynamodb_table.state_verifier_pair.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_dynamodb_policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/python/lambdaFunction.py"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "TuneTally_Lambda_Func" {
+  depends_on    = [data.archive_file.lambda]
+  filename      = "lambda_function_payload.zip"
+  function_name = "TuneTally_Lambda_Func"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambdaFunction.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.13"
+
+  tags = {
+    Project     = "TuneTally_Terraform"
+    Environment = "Production"
+  }
+}
