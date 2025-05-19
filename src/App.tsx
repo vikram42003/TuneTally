a34808df8f2cf47c641aeeb042924f5cde6dd1fe
vikram(wsl) <vikramjit360@gmail.com>
2025-05-19 23:23:33 +0530

    feat: add and setup react-router
    - add a blank page Homepage page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import { spotifyAuth } from "./api/auth/spotifyAuth/spotifyAuth";
+import { Route, Routes } from "react-router";
+
+// import { spotifyAuth as spotifyAuthTest } from "./api/auth/spotifyAuth/spotifyAuth";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
-  const handleSpotifyAuth = async () => {
-    await spotifyAuth();
-  };
+  // const handleSpotifyAuth = async () => {
+  //   await spotifyAuthTest();
+  // };
 
   return (
-    <>
-      <button
-        type="button"
-        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        onClick={handleSpotifyAuth}
-      >
-        Get Stats
-      </button>
-    </>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+
+    // <>
+    //   <button
+    //     type="button"
+    //     className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+    //     onClick={handleSpotifyAuth}
+    //   >
+    //     Get Stats
+    //   </button>
+    // </>
   );
 };
 
