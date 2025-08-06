3bd5d553aca99127a2823cea6b1c1beccddf1ac1
vikram(wsl) <vikramjit360@gmail.com>
2025-08-07 01:28:27 +0530

    feat: change exchangeCodeForTokenAndRedirect to use get/put instead of update_item
    - do that to ensure that we check if the state matchs before making any changes
    - make that function fetch the token from spotify

--- Terraform/python/authorizationLambda.py
+++ Terraform/python/authorizationLambda.py
@@ -3,7 +3,9 @@ import boto3
 import json
 import time
 import uuid
-from urllib import urlencode
+import base64
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 
 SCOPE = "user-read-private user-read-email user-top-read"
@@ -110,27 +112,7 @@ def exchangeCodeForTokenAndRedirect(code, state):
     try:
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("sessionID_token_pair")
-        # response = table.get_item(Key={"sessionID": state})
-        
-        # doc ref - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/table/update_item.html
-        table.update_item(
-            Key={
-                "sessionID": state
-            },
-            updateExpression="SET #token = :t, #expiresAt = :ea",
-            conditionExpression="#sessionID = :state AND #expiresAt > :timenow"
-            expressionAttributeNames={
-                "#sessionID": "sessionID",
-                "#token": "token",
-                "#expiresAt": "expiresAt"
-            },
-            expressionAttributeValues={
-                ":state": state,
-                ":t": auth_token,
-                ":ea": int(time.time()) + 3600
-                ":timenow": int(time.time())
-            }
-        )
+        response = table.get_item(Key={"sessionID": state})
 
         # Check if the returned state matches our saved state (as a security measure)
         if "Item" not in response:
@@ -138,6 +120,65 @@ def exchangeCodeForTokenAndRedirect(code, state):
         # The value for token should be TEMP at this step, if its not then state we might already have a token
         elif response["Item"]["token"] != "TEMP":
             raise Exception("Invalid value for token")
+
+        # Check for redirect uri, client id, and client secret
+        redirect_uri = os.enviorn.get("SPOTIFY_REDIRECT_URI")
+        if not redirect_uri:
+            return error_handler(
+                "Spotify redirect URI not found. Check if enviornment variables are set properly"
+            )
+        client_id = os.enviorn.get("SPOTIFY_CLIENT_ID")
+        if not client_id:
+            return error_handler(
+                "Spotify client ID not found. Check if enviornment variables are set properly"
+            )
+        client_secret = os.enviorn.get("SPOTIFY_CLIENT_SECRET")
+        if not client_secret:
+            return error_handler(
+                "Spotify client secret not found. Check if enviornment variables are set properly"
+            )
+
+        item = response["Item"]
+
+        data = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": redirect_uri,
+        }
+        encoded_data = json.dumps(data).encode("utf-8")
+
+        token_request_url = "https://accounts.spotify.com/api/token"
+        # doc ref - https://docs.python.org/3/library/urllib.request.html and https://stackoverflow.com/a/79435030
+        req = Request(token_request_url, data=encoded_data, method="POST")
+
+        b64_string = base64.b64encode(
+            (client_id + ":" + client_secret).encode("utf-8")
+        ).decode()
+        req.add_header("Authorization", "Basic " + b64_string)
+        req.add_header("Content-Type", "application/x-www-form-urlencoded")
+
+        # This is how you make a request with urllib.request
+        with urlopen(req) as response:
+            res = response.read().decode("utf-8")
+            body = json.loads(res)
+
+            auth_token = body["access_token"]
+            expires_in = body["expires_in"]
+
+        item["token"] = auth_token
+        item["expiresAt"] = expires_in
+
+        table.put_item(Item=item)
+
+        app_base_url = os.environ.get("TUNETALLY_BASE_URL")
+
+        return {
+            "statusCode": 302,
+            "headers": {
+                "Location": app_base_url,
+            },
+        }
+
     except Exception as e:
         return error_handler(e)
 
