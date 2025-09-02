a0df9656c4d9decdad3bfe1145f56f70301e471a
vikram(wsl) <vikramjit360@gmail.com>
2025-09-02 13:20:32 +0530

    feat: add caching related info text to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,12 @@ const Footer = () => {
   return (
     <section className="bg-spotify-dark">
       <div className="layout-container py-8 text-center">
+        The app only stores auth token, profile data, top songs and top artists for caching purposes, but everything gets auto deleted after a duration of 1 hour.
+        <br />
+
         Data is provided by Spotify AB but TuneTally is not related to Spotify AB or any of it's partners in any way
         <br />
+        
         Made with ❤️ by{" "}
         <a href="https://github.com/vikram42003" target="_blank" className="text-blue-400 visited:text-purple-400">
           me!
