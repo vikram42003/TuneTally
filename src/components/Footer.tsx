9addf8b7144e504e3cc15cb33795c29ceb4fa94d
vikram(wsl) <vikramjit360@gmail.com>
2025-09-03 13:14:50 +0530

    feat: change Footer text descriptiveness based on dev mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,18 @@
 const Footer = () => {
+  const mode = import.meta.env.VITE_ENVIRONMENT;
+
   return (
     <section className="bg-spotify-dark">
       <div className="layout-container py-8 text-center">
-        The app only stores auth token, profile data, top songs and top artists for caching purposes, but everything gets auto deleted after a duration of 1 hour.
-        <br />
+        {mode === "dev" ? (
+          ""
+        ) : (
+          <>
+            "The app only stores auth token, profile data, top songs and top artists for caching purposes, but
+            everything gets auto deleted after a duration of 1 hour."
+            <br />
+          </>
+        )}
 
         Data is provided by Spotify AB but TuneTally is not related to Spotify AB or any of it's partners in any way
         <br />
