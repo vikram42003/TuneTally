41134875ad4da80a754a0cf04506bb562dc91a0d
vikram(wsl) <vikramjit360@gmail.com>
2025-09-09 11:55:41 +0530

    feat: style Navbar to be responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   const areWeLoggedInWithAValidSession = !exp ? false : +exp < Date.now();
 
   return (
-    <section className="bg-spotify-dark py-4 text-lg">
+    <section className="bg-spotify-dark py-4 md:text-lg text-xs">
       <nav className="layout-container flex items-center justify-between">
         <div>
-          <Link to="/" className="logo-font navbar-text-shadow flex items-center text-3xl transition-all duration-500">
+          <Link to="/" className="logo-font navbar-text-shadow flex items-center md:text-3xl text-xl transition-all duration-500">
             {/* svg credits - https://www.svgrepo.com/svg/486541/barchart */}
             <img
               src="/images/logo_barchart.svg"
-              className="w-10 -translate-y-1"
+              className="md:w-10 w-8 -translate-y-1"
               alt="app logo thats a green barchart"
             />
             <span className="text-spotify-green">Tune</span>
@@ -23,7 +23,7 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div className="flex items-center gap-12 font-semibold">
+        <div className="flex items-center md:gap-12 gap-2 font-semibold">
           <div>
             <HashLink
               smooth
@@ -63,7 +63,7 @@ const Navbar = () => {
 
           {areWeLoggedInWithAValidSession && (
             <div>
-              <SpotifyButton handler={logoutSpotify} text="Logout" className="px-5! py-1!" />
+              <SpotifyButton handler={logoutSpotify} text="Logout" className="md:px-5! px-4! py-1!" />
             </div>
           )}
         </div>
