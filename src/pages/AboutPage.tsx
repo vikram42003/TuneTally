4e1177cf4c6d6367bcc418c7df8ee14c47bc7f37
vikram(wsl) <vikramjit360@gmail.com>
2025-09-10 01:18:43 +0530

    feat: style AboutPage to be responsive

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router";
 
 const AboutPage = () => {
   return (
-    <section className="bg-spotify-gray">
-      <div className="layout-container space-y-8 pt-16 pb-32 text-base">
-        <h2 className="text-3xl font-bold underline text-spotify-green">About TuneTally</h2>
+    <section className="bg-spotify-gray flex-grow">
+      <div className="layout-container space-y-4 px-6 pt-12 pb-28 text-sm lg:space-y-8 lg:pt-16 lg:pb-32 lg:text-base">
+        <h2 className="text-spotify-green text-2xl font-bold underline lg:text-3xl">About TuneTally</h2>
 
         <p>
           TuneTally is a web app that lets you explore your music listening habits. You can check your top artists,
@@ -29,7 +29,7 @@ const AboutPage = () => {
 
         <p>🎶 Think of it like a mini Spotify Wrapped you can access anytime!</p>
 
-        <p className="text-sm">
+        <p className="text-xs lg:text-sm">
           P.S. If you're a developer, you can check out the code{" "}
           <Link
             to="https://github.com/vikram42003/TuneTally"
@@ -39,7 +39,7 @@ const AboutPage = () => {
           >
             here
           </Link>
-          .{" "}
+          .
         </p>
       </div>
     </section>
